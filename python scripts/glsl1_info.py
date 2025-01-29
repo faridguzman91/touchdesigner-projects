2085a70f13ffb873a03efca1662b5c3a8e78e66f
faridguzman91 <farid.guzman91@hotmail.com>
2025-01-29 21:23:47 +0100

    added scripts

---
+++ python scripts/glsl1_info.py
@@ -0,0 +1,15 @@
+Vertex Shader Compile Results: 
+
+Compiled Successfully
+
+=============
+
+Pixel Shader Compile Results: 
+
+Compiled Successfully
+
+=============
+
+Program Link Results: 
+
+Linked Successfully
